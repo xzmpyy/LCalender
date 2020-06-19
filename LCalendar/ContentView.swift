825deccf993bcc21148c0567adfa577614dbf291
xzmpyy <zj0714zj@163.com>
2020-06-19 13:35:39 +0800

    Initial Commit

---
+++ LCalendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LCalendar
+//
+//  Created by 张捷 on 2020/6/19.
+//  Copyright © 2020 张捷. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
